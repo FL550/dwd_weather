ba6d53e1dead57adce142782e0cb0c835c72597e
FL550 <max.fermor@gmail.com>
2023-06-23 13:56:11 +0200

    Removes typing

--- custom_components/dwd_weather/const.py
+++ custom_components/dwd_weather/const.py
@@ -1,20 +1,19 @@
 """Constants for the DWD Weather integration."""
 from datetime import timedelta
-from typing import Final
 
 from homeassistant.const import Platform
 
-DOMAIN: Final = "dwd_weather"
+DOMAIN = "dwd_weather"
 # Platforms
-PLATFORMS: Final = [
+PLATFORMS = [
     Platform.SENSOR,
     Platform.WEATHER,
 ]
-INTEGRATION_VERSION: Final = "1.2.28"
-MIN_REQUIRED_HA_VERSION: Final = "2022.7.1"
+INTEGRATION_VERSION = "1.2.28"
+MIN_REQUIRED_HA_VERSION = "2022.7.1"
 
-DEFAULT_NAME: Final = "DWD Weather"
-ATTRIBUTION: Final = "Data provided by Deutscher Wetterdienst (DWD)"
+DEFAULT_NAME = "DWD Weather"
+ATTRIBUTION = "Data provided by Deutscher Wetterdienst (DWD)"
 ATTR_LATEST_UPDATE = "latest_update_utc"
 ATTR_ISSUE_TIME = "forecast_time_utc"
 ATTR_STATION_ID = "station_id"
