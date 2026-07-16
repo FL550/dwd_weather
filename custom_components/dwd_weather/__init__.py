f817f34126ca0ba02e79742c5dd54c21475d506a
torst3n <torst3n@users.noreply.github.com>
2026-07-16 21:39:21 +0200

    Fetch initial map data/images on startup by refreshing map coordinator in async_setup_entry

--- custom_components/dwd_weather/__init__.py
+++ custom_components/dwd_weather/__init__.py
@@ -120,6 +120,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
             DWDWEATHER_COORDINATOR: dwdweather_coordinator,
         }
 
+        # Fetch initial map data/images on startup
+        await dwdweather_coordinator.async_refresh()
+
         await hass.config_entries.async_forward_entry_setups(entry, ["camera"])
 
     return True
